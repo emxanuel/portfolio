bd5fcd7bf7c8d4f6efc3f54eb0aa60fadda0cfca
Emmanuel Lopez Garcia <emmanuel@kali.kali>
2023-07-10 17:07:07 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <div className='h-56 bg-slate-900'>
+          <h1 className='font-bold text-5xl text-center justify-center text-white opacity
+          
+          '>
+            Emmanuel Lopez
+          </h1>
+          <h2 className='text-slate-400 text-4xl font-semibold text-center opacity-90'>
+            portfolio
+          </h2>
+      </div>
+      <div className='h-52 bg-slate-900 rotate-6 -mt-28 skew-x-6 w-full'>
+      </div>
+    </div>
+
+  )
+}
+
+export default App
