488bf82612590ad990251cb3f69362d8d7d3b077
emxanuel <ealg0528@gmail.com>
2023-07-10 21:27:26 -0400

    add: social networks and projects section

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
+    <div className='select-none min-h-screen flex flex-col'>
         <div className='h-56 bg-slate-900'>
-          <h1 className='font-bold text-5xl text-center justify-center text-white opacity
-          
+          <h1 className='font-bold text-5xl text-center text-white
+          pt-6
           '>
             Emmanuel Lopez
           </h1>
@@ -13,7 +13,30 @@ const App = () => {
             portfolio
           </h2>
       </div>
-      <div className='h-52 bg-slate-900 rotate-6 -mt-28 skew-x-6 w-full'>
+      <div className='z-0 h-52 bg-slate-900 rotate-6 -mt-24 skew-x-6 -mr-1 -ml-1 justify-center'>
+      </div>
+      <div className='-mt-20 font-semibold text-white relative  '>
+        <h1 className=' text-center w-screen text-7xl relative
+        '>Projects</h1>
+        <div className='w-screen justify-center flex flex-col items-center p-11'>
+          <a href="https://emxanuel.github.io/social-network" target='_blank' className='text-slate-500 text-3xl'>
+            social network
+          </a>
+          <div className='bg-black opacity-80 w-52 aspect-video flex justify-center text-center items-center'>
+            <p className=''>preview</p>
+          </div>
+        </div>
+      </div>
+      <div className='flex relative justify-center gap-8 '>
+        <a target='_blank' href='https://github.com/emxanuel'>
+          <i className='fa-brands fa-github text-5xl hover:text-gray-400'/>
+        </a>
+        <a target='_blank' href="https://instagram.com/emmanueellllll">
+          <i className='fa-brands fa-instagram text-5xl hover:text-gray-400'/>
+        </a>
+        <a href="" target='_blank'>
+          <i className='fa-brands fa-facebook text-5xl hover:text-gray-400' />
+        </a>
       </div>
     </div>
 
